818ac5a81bca64b07cc8327c2c919a883c002ac2
oktapradika57-maker <oktapradika57@gmail.com>
2026-09-11 11:23:42 +0700

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ from plotly.subplots import make_subplots
 # 1. KONFIGURASI HALAMAN & CSS STYLING
 # ==========================================
 st.set_page_config(
-    page_title="Dashboard SIRAPI - PM Monitoring",
+    page_title="Dashboard SiPM - PM Monitoring",
     page_icon="📈",
     layout="wide",
     initial_sidebar_state="expanded"
